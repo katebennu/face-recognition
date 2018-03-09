7ab412152284c9b45c40f5a35bef95a96503152d
katebennu <kate.dorrer@gmail.com>
2018-03-09 23:44:01 +0200

    update path to test, add gitignore
    Author:    katebennu <kate.dorrer@gmail.com>

--- recognize.py
+++ recognize.py
@@ -5,15 +5,19 @@ import sys
 known_labels = []
 known_faces = []
 
-for file in os.listdir(os.getcwd() + "/" + sys.argv[1] + "/learning"):
-    face = face_recognition.load_image_file(os.getcwd() + "/" + sys.argv[1] + "/learning/" + file)
+base_path = os.getcwd() + "/" + sys.argv[1]
+learning_path = base_path + "/learning/"
+test_path = base_path + "/test/"
+
+for file in os.listdir(learning_path):
+    face = face_recognition.load_image_file(learning_path + file)
     label = file.split('.')[0]
 
     known_labels.append(label)
     known_faces.append(face_recognition.face_encodings(face)[0])
 
 
-unknown_image = face_recognition.load_image_file(os.getcwd() + "/obama-trump_separated/test/obama2.jpg")
+unknown_image = face_recognition.load_image_file(test_path + os.listdir(test_path)[0])
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
